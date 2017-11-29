447aba8ad427cacd77e51603ad07ab75faad1beb
Raj Sharma <rajsharma3@gmail.com>
2017-11-30 01:55:47 +0530

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## Objective is to make a cache matrix from a given matrix and establish its mean
+## It comprises of two steps
+## First Step creates the matrix that can cache its inverse
+## Second Step returns the value of the inverse if calculated in Setp 1
+
+##---------------------------------------------------------------
+
+## Step 1 : Create cache of inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cacheMatrix <- NULL  ## Initialising cache matrix and assigning Null Value
 
+  ## Establishing the definition 
+  set <- function(y) {
+    x <<- y
+    cacheMatrix <<- NULL
+  }
+  get <- function() x
+  
+  ## definition Setcache
+  
+  setcache <- function(inverse) cacheMatrix <<- inverse
+  
+  ## definition Getcache
+  
+  getcache <- function() cacheMatrix
+  
+  ## Listing the methods
+  
+  list(set = set,
+       get = get,
+       setcache = setcache,
+       getcache = getcache)
+  
 }
 
+##-----------------------------------------------------------------------------
 
-## Write a short comment describing this function
+## Step 2: Return the value of inverse in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## checking the content
+  
+  cacheMatrix <- x$getCache()
+
+  ## returning the results
+  
+  if (!is.null(cacheMatrix)) {
+      return(cacheMatrix)
+  }
+  
+  ## Calculating alternate result
+  
+  else {
+    dMatrix <- x$getMatrix()
+    cacheMatrix <- solve(dMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  
+}
+
 }
